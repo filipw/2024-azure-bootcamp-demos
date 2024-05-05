c03da4adcab63c4d3bf5774988ca92c4a40de820
filipw <filip@strathweb.com>
2024-05-05 11:54:54 +0200

    extracted vectorization to a separate step

--- sample-chat/do_search.py
+++ sample-chat/do_search.py
@@ -2,7 +2,6 @@ import os
 import requests
 from typing import List
 from promptflow.core import tool
-from promptflow.contracts.multimedia import Image as PFImage
 from azure.search.documents import SearchClient
 from azure.search.documents.models import (
     VectorizedQuery,
@@ -11,28 +10,31 @@ from azure.search.documents.models import (
     QueryAnswerType,
 )
 from azure.core.credentials import AzureKeyCredential
-from promptflow.connections import (
-    CognitiveSearchConnection,
-    CustomConnection
-)
+from promptflow.connections import CognitiveSearchConnection
 
 @tool
 def do_search(
     question: str,
     index_name: str,
-    #embedding: List[float],
     search: CognitiveSearchConnection,
-    vision: CustomConnection,
-    image: PFImage = None,
+    image_vector: List[float],
 ) -> str:
+    search_client = SearchClient(
+        endpoint=search.configs["api_base"],
+        index_name=index_name,
+        credential=AzureKeyCredential(search.secrets["api_key"]),
+    )
+
+    vector_queries = None
+    if image_vector:
+        vector_queries = [VectorizedQuery(vector=image_vector, k_nearest_neighbors=3, fields="image_vector")]
+
+    results = search_client.search(  
+        search_text=question,  
+        vector_queries=vector_queries,
+        select=["description", "filepath"],
+    )  
     
-    image_vector = None
-    if image is not None:
-        vector_data = vectorize_image(image, vision.configs["api_base"], vision.secrets["api_key"])
-        image_vector = vector_data['vector']
-
-    results = perform_image_search(search, index_name, question, image_vector)
-
     docs = [
         {
             "description": doc["description"]
@@ -41,37 +43,3 @@ def do_search(
     ]
 
     return docs
-
-def vectorize_image(image_data, endpoint, subscription_key):
-    api_url = f"{endpoint}/computervision/retrieval:vectorizeImage?api-version=2024-02-01&model-version=2023-04-15"
-    
-    headers = {
-        "Content-Type": "application/octet-stream",
-        "Ocp-Apim-Subscription-Key": subscription_key
-    }
-
-    response = requests.post(api_url, headers=headers, data=image_data)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception(f"API call failed with status code {response.status_code}: {response.text}")
-
-def perform_image_search(search, index_name, text_query, image_vector=None):
-    search_client = SearchClient(
-        endpoint=search.configs["api_base"],
-        index_name=index_name,
-        credential=AzureKeyCredential(search.secrets["api_key"]),
-    )
-
-    vector_queries = None
-    if image_vector is not None:
-        vector_queries = [VectorizedQuery(vector=image_vector, k_nearest_neighbors=3, fields="image_vector")]
-
-    results = search_client.search(  
-        search_text=text_query,  
-        vector_queries=vector_queries,
-        select=["description", "filepath"],
-    )  
-    
-    return results
---
+++ sample-chat/vectorize_image.py
@@ -0,0 +1,27 @@
+import requests
+from typing import List
+from promptflow.core import tool
+from promptflow.contracts.multimedia import Image as PFImage
+from promptflow.connections import CustomConnection
+
+@tool
+def vectorize_image(vision: CustomConnection, image: PFImage = None) -> List[float]:
+    if image is not None:
+        endpoint = vision.configs["api_base"]
+        subscription_key = vision.secrets["api_key"]
+        api_url = f"{endpoint}/computervision/retrieval:vectorizeImage?api-version=2024-02-01&model-version=2023-04-15"
+        
+        headers = {
+            "Content-Type": "application/octet-stream",
+            "Ocp-Apim-Subscription-Key": subscription_key
+        }
+
+        response = requests.post(api_url, headers=headers, data=image)
+
+        if response.status_code == 200:
+            vector_data = response.json()
+            return vector_data['vector']
+        else:
+            raise Exception(f"API call failed with status code {response.status_code}: {response.text}")
+    
+    return []
